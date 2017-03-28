0ad8569d873f76833d2445684d9fc52cc2fbaa7f
wilson <wilsonskariah@wilsons-MBP.fios-router.home>
2017-03-27 23:32:42 -0400

    update foldes

--- coretuxapp/src/com/codebind/coretuxapp.java
+++ coretuxapp/src/com/codebind/coretuxapp.java
@@ -1,7 +1,31 @@
 package com.codebind;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by wilsonskariah on 3/12/17.
  */
 public class coretuxapp {
+    private JButton Tux_launch;
+    private JPanel Tuxpanel;
+
+    public coretuxapp() {
+        Tux_launch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Brandon is soo coool");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame ("coretuxapp");
+        frame.setContentPane(new coretuxapp().Tuxpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
